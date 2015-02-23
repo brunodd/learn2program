e83de516167a0b69a69eb9054e3bd10008498664
arminnh <halilovic.armin@hotmail.com>
2015-02-23 07:32:15 -0800

    Modified gulpfile, downloaded scripts so theyre stored locally

--- htdocs/gulpfile.js
+++ htdocs/gulpfile.js
@@ -12,5 +12,16 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.sass('app.scss', 'resources/css');
+
+    mix.styles([
+        'libs/bootstrap.min.css',
+        'app.css',
+        'libs/select2.min.css'
+    ]);
+
+    mix.scripts([
+        'libs/jquery.js',
+        'libs/select2.min.js'
+    ]);
 });
