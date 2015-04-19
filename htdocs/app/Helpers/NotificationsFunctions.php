59f38b062270b46a7fd42c82eb5d081cb764bb67
Armin <halilovic.armin@hotmail.com>
2015-04-19 12:10:48 +0200

    Fixed indent

--- htdocs/app/Helpers/NotificationsFunctions.php
+++ htdocs/app/Helpers/NotificationsFunctions.php
@@ -2,30 +2,30 @@
 
 function loadAllNotifications() {
     return \DB::select('SELECT *
-                            FROM notifications
-                            WHERE userId = ?',
-        [\Auth::id()]);
+                        FROM notifications
+                        WHERE userId = ?',
+                        [\Auth::id()]);
 }
 
 function loadUnreadNotifications() {
     return \DB::select('SELECT *
-                            FROM notifications
-                            WHERE userId = ? and is_read = 0',
-        [\Auth::id()]);
+                        FROM notifications
+                        WHERE userId = ? and is_read = 0',
+                        [\Auth::id()]);
 }
 
 function loadLastNNotifications($n) {
     return \DB::select('SELECT *
-                            FROM notifications
-                            WHERE userId = ?
-                            ORDER BY id DESC
-                            LIMIT ?',
-        [\Auth::id(), $n]);
+                        FROM notifications
+                        WHERE userId = ?
+                        ORDER BY id DESC
+                        LIMIT ?',
+                        [\Auth::id(), $n]);
 }
 
 function updateNotificationsToSeen() {
     return \DB::statement('UPDATE   notifications
-                               SET      is_read = true
-                               WHERE    userId = ?',
-        [\Auth::id()]);
+                           SET      is_read = true
+                           WHERE    userId = ?',
+                           [\Auth::id()]);
 }
--- htdocs/app/Helpers/RecommendationsFunctions.php
+++ htdocs/app/Helpers/RecommendationsFunctions.php
@@ -2,18 +2,18 @@
 
 function returnSeriesSameMaker($serie) {
     return DB::select('SELECT *
-                            FROM series
-                            WHERE series.title != ? and makerId = ?',
-        [$serie->title, $serie->makerId]);
+                       FROM series
+                       WHERE series.title != ? and makerId = ?',
+                       [$serie->title, $serie->makerId]);
 }
 
-function returnSeriesSameDifficulty($serie) {
-    $difficulty = DB::select('SELECT *
-                                FROM types
-                                WHERE types.id = ?', [$serie->tId]);
+    function returnSeriesSameDifficulty($serie) {
+        $difficulty = DB::select('SELECT *
+                                  FROM types
+                                  WHERE types.id = ?', [$serie->tId]);
 
-    return DB::select(' SELECT *
+        return DB::select(' SELECT *
                             FROM series, types
                             WHERE series.title != ? and series.tId = types.id and types.difficulty = ?',
-        [$serie->title, $difficulty[0]->difficulty]);
-}
+                            [$serie->title, $difficulty[0]->difficulty]);
+    }
