6956e37141f14e3b5d122f32572b5487d66f32d9
Fouad <fouad.k@outlook.be>
2015-04-23 16:15:58 +0200

    Recommendations also by rating

--- htdocs/app/Helpers/RecommendationsFunctions.php
+++ htdocs/app/Helpers/RecommendationsFunctions.php
@@ -19,25 +19,31 @@ function returnSeriesSameDifficulty($serie) {
                         and types.difficulty = ?',
                         [$serie->title, $difficulty[0]->difficulty]);
 }
-/*
+
 function returnSeriesSameRating($serie) {
     $rating = DB::select('SELECT *
                           FROM series, series_ratings
                           WHERE series.id = series_ratings.seriesId and series.id = ?', [$serie->id]);
-    return !empty(DB::select( 'SELECT *
+    if (!empty($rating)) {
+        return DB::select( 'SELECT *
                         FROM series, series_ratings 
                         WHERE series.id = series_ratings.seriesId 
                         and series.id != ?
                         and series_ratings.rating = ? ',
-                        [$serie->id, $rating[0]->rating]));
+                        [$serie->id, $rating[0]->rating]); 
+  } else {
+    return DB::select('SELECT *
+                       FROM seriesId
+                       WHERE series.id != series.id');
+  }
 }
-*/
+
 function returnRecommendations($serie) {
   $sameMaker = returnSeriesSameMaker($serie);
   $sameDifficulty = returnSeriesSameDifficulty($serie);
-  //$sameRating = returnSeriesSameRating($serie);
+  $sameRating = returnSeriesSameRating($serie);
 
-  $result = $sameDifficulty + $sameMaker;
+  $result = $sameDifficulty + $sameMaker + $sameRating;
 
   return $result;
 }
