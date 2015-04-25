1fc29e9c7ab675ece25d3d9bab73023bba44fa3e
brunodd <dedekenbruno@gmail.com>
2015-04-25 10:15:32 +0200

    Made reference exercise fields: readonly

--- htdocs/resources/views/exercises/reference.blade.php
+++ htdocs/resources/views/exercises/reference.blade.php
@@ -43,22 +43,22 @@
 
         <div class="form-group">
             {!! Form::label('question', 'Question: ') !!}
-            {!! Form::text('question', null, ['class' => 'form-control']) !!}
+            {!! Form::text('question', null, ['class' => 'form-control', 'readonly']) !!}
         </div>
 
         <div class="form-group">
             {!! Form::label('start_code', 'Starting code, i.e. this code will initially be given to the user: ') !!}
-            {!! Form::textarea('start_code', null, ['class' => 'form-control']) !!}
+            {!! Form::textarea('start_code', null, ['class' => 'form-control', 'readonly']) !!}
         </div>
 
         <div class="form-group">
             {!! Form::label('tips', 'Tips: ') !!}
-            {!! Form::textarea('tips', null, ['class' => 'form-control']) !!}
+            {!! Form::textarea('tips', null, ['class' => 'form-control', 'readonly']) !!}
         </div>
 
         <div class="form-group">
             {!! Form::label('expected_result', 'Expected result, i.e. the output of the program: ') !!}
-            {!! Form::textarea('expected_result', null, ['class' => 'form-control']) !!}
+            {!! Form::textarea('expected_result', null, ['class' => 'form-control', 'readonly']) !!}
         </div>
 
 
