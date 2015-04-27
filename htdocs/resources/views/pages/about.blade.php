178c3ac3ab86e7e0e2071adf60371ff3b7fafc42
Fouad <fouad.k@outlook.be>
2015-04-27 10:54:28 +0200

    changed the about-page a little bit more

--- htdocs/resources/views/pages/about.blade.php
+++ htdocs/resources/views/pages/about.blade.php
@@ -12,7 +12,7 @@
 <div id="owl-demo" class="owl-carousel text-center">
     <div class="item">
         <div class="cau_left">
-            <img class="lazyOwl" data-src="/images/web/armin2.png" alt="Lazy Owl Image">
+            <img class="lazyOwl" data-src="/images/web/armin2-round.png" alt="Lazy Owl Image">
         </div>
         <div class="cau_left">
             <br><h4><a href="#">Armin Halilovic</a></h4>
@@ -23,7 +23,7 @@
     </div>
     <div class="item">
         <div class="cau_left">
-            <img class="lazyOwl" data-src="/images/web/fouad2.png" alt="Lazy Owl Image">
+            <img class="lazyOwl" data-src="/images/web/fouad2-round.png" alt="Lazy Owl Image">
         </div>
         <div class="cau_left">
             <br>
@@ -35,10 +35,10 @@
     </div>
     <div class="item">
         <div class="cau_left">
-            <img class="lazyOwl" data-src="/images/web/raphael2.png" alt="Lazy Owl Image">
+            <img class="lazyOwl" data-src="/images/web/raphael2-round.png" alt="Lazy Owl Image">
         </div>
         <div class="cau_left">
-            <br><h4><a href="#">Raphael </a></h4>
+            <br><h4><a href="#">Raphael Assa</a></h4>
             <p>
                 Database engineer.
             </p>
@@ -46,7 +46,7 @@
     </div>
     <div class="item">
         <div class="cau_left">
-            <img class="lazyOwl" data-src="/images/web/bruno2.png" alt="Lazy Owl Image">
+            <img class="lazyOwl" data-src="/images/web/bruno2-round.png" alt="Lazy Owl Image">
         </div>
         <div class="cau_left">
             <br><h4><a href="">Bruno De Deken</a></h4>
