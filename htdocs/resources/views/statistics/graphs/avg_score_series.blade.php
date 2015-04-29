c0e20ef725eafd9d54ea4ef71913ce904e70f439
brunodd <dedekenbruno@gmail.com>
2015-04-29 13:23:02 +0200

    Fixed average score to ignore scores = 0%

--- htdocs/resources/views/statistics/graphs/avg_score_series.blade.php
+++ htdocs/resources/views/statistics/graphs/avg_score_series.blade.php
@@ -1,36 +1,66 @@
 <!-- brunodd: Get data from database -->
 <?php
-    $total = countExercisesBySeries();
+    $num_exercises_per_series = countExercisesBySeries();
     $users = loadusers();
     if( count($users) > 0 ) {
-        $avgs = countUserSucceededExercisesBySeries($users[0]->id);
-        if( count($avgs) > 0 ) {
-            for( $i = 1; $i < count($users); $i++ ) {
-                    $userScores = countUserSucceededExercisesBySeries($users[$i]->id);
-                    // Length of $userScores & $avgs SHOULD BE THE SAME! Otherwise something must have gone horribly wrong
-                    for( $j=0; $j < count($avgs); $j++ ) {
-                    // Again a "useless" safety check since this should allways match
-                    if( $avgs[$j]->seriesId == $userScores[$j]->seriesId ) {
-                        $avgs[$j]->c += $userScores[$j]->c;
-                        }
-                    }
-            }
-            // Again, length of $total & $avgs SHOULD BE THE SAME!
-            for( $i=0; $i < count($avgs); $i++ ) {
-                if( $total[$i]->seriesId == $avgs[$i]->seriesId && $total[$i]->c > 0 ) {
-                    $avgs[$i]->c = $avgs[$i]->c / ($total[$i]->c * count($users));
-                }
-                elseif( $total[$i]->seriesId == $avgs[$i]->seriesId && $total[$i]->c == 0 ) $avgs[$i]->c = -1;
-            }
+        $avg_scores_per_series = countUserSucceededExercisesBySeries($users[0]->id);
+        foreach($avg_scores_per_series as $entry) {
+            $entry->c = 0;
         }
-    }
-    // dd($avgs);
-    $series = [];
-    $scores = [];
-    foreach($avgs as $a) {
-        if($a->c >= 0) {
-            array_push($series, loadSerieWithId($a->seriesId)[0]->title);
-            array_push($scores, ($a->c * 100));
+        if( count($avg_scores_per_series) > 0 ) {
+
+            // Get an array of the number of users that tried to solve the
+            // series.
+            $active_users_per_series = [];
+            foreach($num_exercises_per_series as $ex) {
+                array_push($active_users_per_series, 0);
+            }
+
+            // Fill avg_scores_per_series[] with the amount of correct
+            // exercises (over all users).
+            foreach($users as $user) {
+                $user_scores_per_series = countUserSucceededExercisesBySeries($user->id);
+                // dd(countUserSucceededExercisesBySeries(1));
+                // Length of $user_scores_per_series & $avg_scores_per_series SHOULD BE THE SAME! Otherwise something must have gone horribly wrong
+                // Iterate over all series.
+                for( $j = 0; $j < count($avg_scores_per_series); $j++ ) {
+                    // Again a "useless" safety check since this should allways match
+                    if( $avg_scores_per_series[$j]->seriesId == $user_scores_per_series[$j]->seriesId ) {
+                        $avg_scores_per_series[$j]->c += $user_scores_per_series[$j]->c;
+                    }
+                    if( $user_scores_per_series[$j]->c > 0) {
+                        $active_users_per_series[$j] += 1;
+                    }
+                }
+            }
+            // dd($active_users_per_series);
+            // dd($avg_scores_per_series);
+
+            // Calculate the average scores in avg_score_per_series[].
+            for( $i = 0; $i < count($avg_scores_per_series); $i++ ) {
+                // Protection against division by 0.
+                if($active_users_per_series[$i] == 0) {
+                    $active_users_per_series[$i] = 1;
+                }
+
+                if( $num_exercises_per_series[$i]->seriesId == $avg_scores_per_series[$i]->seriesId && $num_exercises_per_series[$i]->c > 0 ) {
+                    // $avg_scores_per_series[$i]->c = $avg_scores_per_series[$i]->c / ($num_exercises_per_series[$i]->c * count($users));
+                    $avg_scores_per_series[$i]->c = $avg_scores_per_series[$i]->c / ($num_exercises_per_series[$i]->c * $active_users_per_series[$i]);
+                }
+                elseif( $num_exercises_per_series[$i]->seriesId == $avg_scores_per_series[$i]->seriesId && $num_exercises_per_series[$i]->c == 0 ) {
+                    $avg_scores_per_series[$i]->c = -1;
+                }
+            }
+
+            // dd($avg_scores_per_series);
+            $series = [];
+            $scores = [];
+            foreach($avg_scores_per_series as $a) {
+                if($a->c >= 0) {
+                    array_push($series, loadSerieWithId($a->seriesId)[0]->title);
+                    array_push($scores, ($a->c * 100));
+                }
+            }
         }
     }
 
