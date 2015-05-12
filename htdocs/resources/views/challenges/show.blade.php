cb5da509f72599d366ad1a220dd09395d334e5bd
brunodd <dedekenbruno@gmail.com>
2015-05-12 15:26:08 +0200

    Improved single challenge's page

--- htdocs/resources/views/challenges/show.blade.php
+++ htdocs/resources/views/challenges/show.blade.php
@@ -4,18 +4,40 @@
 <?php
     $userA = loadUser($challenge->userA)[0];
     $userB = loadUser($challenge->userB)[0];
+    $winner = [];
+    $loser = [];
+    if($challenge->winner == $userA->id) {
+        $winner = $userA;
+        $loser = $userB;
+    }
+    else if ($challenge->winner == $userB->id) {
+        $winner = $userB;
+        $loser = $userA;
+    }
 ?>
-Exercise {{ $challenge->exId }}: {{ $userA->username }} vs {{ $userB->username }}
+<u><a href="/exercises/{{$challenge->exId}}"><font color="white">Exercise {{ $challenge->exId }}</font></a></u>: {{ $userA->username }} vs {{ $userB->username }}
 @stop
 
 @section('content')
-@if ($challenge->winner != NULL)
-<h2> Winner: {{ loadUser($challenge->winner)[0]->username }} </h2>
-@else
-<h2> Tie </h2>
-@endif
+<div>
+    @if ($winner == [])
+        <h2> Tie </h2>
 
-<u><a href="/exercises/{{$challenge->exId}}">Complete exercise</a></u>
+    @else
+    <div>
+        <h2> Winner: {{ $winner->username }} </h2>
+        ({{ loadAnswers($winner->id, $challenge->exId)[0]->time }} seconds)
+    </div>
+    <div>
+        <h3> Loser: {{ $loser->username }} </h3>
+            @if(empty(loadAnswers($loser->id, $challenge->exId)))
+                (not tried yet)
+            @else
+                ({{ loadAnswers($loser->id, $challenge->exId)[0]->time }} seconds)
+            @endif
+    </div>
+    @endif
+</div>
 
 @stop
 
