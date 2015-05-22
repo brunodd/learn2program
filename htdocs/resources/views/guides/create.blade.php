2b1cb9290bc72debbb71b1b1137edbf4c56b6a57
Raffson <raphael@Raphaels-MacBook-Pro.local>
2015-05-22 04:10:37 +0200

    Added decent editor for Guides

--- htdocs/resources/views/guides/create.blade.php
+++ htdocs/resources/views/guides/create.blade.php
@@ -1,11 +1,25 @@
 @extends('master')
 
+@section('head')
+    <script src="/js/tinymce/tinymce.min.js"></script>
+    <script>tinymce.init({
+        selector: "textarea",
+        theme: "modern",
+        plugins: [
+            "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime media nonbreaking save table contextmenu directionality",
+            "emoticons template paste textcolor colorpicker textpattern"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons" });</script>
+@stop
+
 @section('title')
     Create a new guide
 @stop
 
 @section('content')
-<div class="container col-md-8 col-md-offset-2">
+<div class="container col-md-10 col-md-offset-1">
     {!! Form::open(['url' => '/guides']) !!}
         <div class="form-group">
             {!! Form::label('title', 'Title: ') !!}
--- htdocs/resources/views/guides/edit.blade.php
+++ htdocs/resources/views/guides/edit.blade.php
@@ -1,12 +1,26 @@
 @extends('master')
 
+@section('head')
+    <script src="/js/tinymce/tinymce.min.js"></script>
+    <script>tinymce.init({
+        selector: "textarea",
+        theme: "modern",
+        plugins: [
+            "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime media nonbreaking save table contextmenu directionality",
+            "emoticons template paste textcolor colorpicker textpattern"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons" });</script>
+@stop
+
 @section('title')
     Edit <em>{{ $guide->title }}</em>
 @stop
 
 @section('content')
     <!-- TODO: armin PUT vs PATCH -->
-    <div class="container col-md-8 col-md-offset-2">
+    <div class="container col-md-10 col-md-offset-1">
         {!! Form::model($guide, ['url' => '/guides/' . $guide->title, 'method' => 'PUT']) !!}
             <div class="form-group">
                 {!! Form::label('content', 'Content: ') !!}
