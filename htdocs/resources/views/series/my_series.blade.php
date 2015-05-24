80f1a15a9c6087dcdccaaf364442739fe5df45d6
Armin <halilovic.armin@hotmail.com>
2015-05-24 14:39:20 +0200

    Revert

--- htdocs/resources/views/series/my_series.blade.php
+++ htdocs/resources/views/series/my_series.blade.php
@@ -39,7 +39,7 @@
         <div class="series" id="mix-wrapper">
             @foreach($series as $serie)
                 @if( SerieContainsExercises($serie->id) || isMakerOfSeries($serie->id, Auth::id()) )
-                    <div class="mix ttr {{ loadType2($serie->tId)[0]->difficulty }}" data-title="{{$serie->title}}" data-rating="{{ loadRatingAsInt($serie->id) }}" data-subject="{{ loadType2($serie->tId)[0]->subject }}" data-difficulty="{{ loadDifficultyAsInt($serie->tId) }}" onclick="window.location.href='/series/{{$serie->title}}';">
+                    <div class="mix ttr {{ loadType2($serie->tId)[0]->difficulty }}" data-title="{{$serie->title}}" data-rating="{{ loadRatingAsInt($serie->id) }}" data-subject="{{ loadType2($serie->tId)[0]->subject }}" data-difficulty="{{ loadDifficultyAsInt($serie->tId) }}" onclick="window.location.href='/series/{{$serie->id}}';">
                         <div class="ttd">{{$serie->title}}</div>
                         <div class="ttd dd">{{ averageRating($serie->id) }}</div>
                         <div class="ttd dd">{{ loadType2($serie->tId)[0]->subject }}</div>
