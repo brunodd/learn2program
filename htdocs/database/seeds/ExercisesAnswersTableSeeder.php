713b837de9060cb70523f0d6a94e8196359c91df
brunodd <dedekenbruno@gmail.com>
2015-05-25 14:00:54 +0200

    Added answer seeds

--- htdocs/database/seeds/ExercisesAnswersTableSeeder.php
+++ htdocs/database/seeds/ExercisesAnswersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Answer;
 
 class ExercisesAnswersTableSeeder extends Seeder {
 
@@ -8,5 +9,37 @@ class ExercisesAnswersTableSeeder extends Seeder {
         DB::table('answers')->delete();
         DB::statement('ALTER TABLE answers AUTO_INCREMENT=1');
 
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 1, 1)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 2, 1)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 2, 2)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 1, 5)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 1, 3)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 4, 5)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 10, 10)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 2, 3)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 9, 1)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 2, 10)');
+
+        DB::insert('INSERT INTO ANSWERS (given_code, success, uId, eId)
+                                VALUES ("xx", true, 1, 7)');
     }
 }
