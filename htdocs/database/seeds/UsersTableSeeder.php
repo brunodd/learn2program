41984c821ec0c86fc96f56301db2c9ab9c6b3851
Fouad <fouad.k@outlook.be>
2015-05-25 13:03:20 +0200

    updated users with 'real' name

--- htdocs/database/seeds/UsersTableSeeder.php
+++ htdocs/database/seeds/UsersTableSeeder.php
@@ -14,7 +14,7 @@ class UsersTableSeeder extends Seeder {
             'image' => 'user1ProfileImage.jpg', 'info' => 'Armin Halilovic is the driving force.']);
         User::create(['username' => 'bruno', 'mail' => 'b@b.b', 'pass' => bcrypt('bruno'), 'score' => '10',
             'image' => 'user2ProfileImage.jpg', 'info' => 'Bruno De Deken is our main design architect.']);
-        User::create(['username' => 'raphael', 'mail' => 'r@r.r', 'pass' => bcrypt('raphael'), 'score' => '0',
+        User::create(['username' => 'raphael', 'mail' => 'r@r.r', 'pass' => bcrypt('raphael'), 'score' => '8',
             'image' => 'user3ProfileImage.jpg', 'info' => "<h3 class=\"container col-md-9 col-md-offset-1\">Introduction:</h3>
 <div class=\"container col-md-9 col-md-offset-1\">Raphael Assa is the database designer. Sometimes he spends his time on useless&nbsp;stuff like listening to some music through his own profile page<img src=\"../../js/tinymce/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\"><iframe src=\"https://www.youtube.com/embed/QMKWwOe7FL4\" width=\"425\" height=\"350\"></iframe></div>
 <div class=\"container col-md-9 col-md-offset-1\">&nbsp;</div>
@@ -29,38 +29,47 @@ class UsersTableSeeder extends Seeder {
 <li><em>Re-use the first 3 steps &amp; overcome all problems of humanity</em></li>
 </ol>
 </div> "]);
-        User::create(['username' => 'fouad', 'mail' => 'f@f.f', 'pass' => bcrypt('fouad'), 'score' => '0', 
+        User::create(['username' => 'fouad', 'mail' => 'f@f.f', 'pass' => bcrypt('fouad'), 'score' => '6', 
             'image' => 'user4ProfileImage.jpg', 'info' => 'Fouad is the graphic designer.']);
-        User::create(['username' => 'u0', 'mail' => 'u0@x.x', 'pass' => bcrypt('u0'), 'score' => '0']);
-        User::create(['username' => 'u1', 'mail' => 'u1@x.x', 'pass' => bcrypt('u1'), 'score' => '0']);
-        User::create(['username' => 'u2', 'mail' => 'u2@x.x', 'pass' => bcrypt('u2'), 'score' => '0']);
-        User::create(['username' => 'u3', 'mail' => 'u3@x.x', 'pass' => bcrypt('u3'), 'score' => '0']);
-        User::create(['username' => 'u4', 'mail' => 'u4@x.x', 'pass' => bcrypt('u4'), 'score' => '0']);
-        User::create(['username' => 'u5', 'mail' => 'u5@x.x', 'pass' => bcrypt('u5'), 'score' => '0']);
-        User::create(['username' => 'u6', 'mail' => 'u6@x.x', 'pass' => bcrypt('u6'), 'score' => '0']);
-        User::create(['username' => 'u7', 'mail' => 'u7@x.x', 'pass' => bcrypt('u7'), 'score' => '0']);
-        User::create(['username' => 'u8', 'mail' => 'u8@x.x', 'pass' => bcrypt('u8'), 'score' => '0']);
-        User::create(['username' => 'u9', 'mail' => 'u9@x.x', 'pass' => bcrypt('u9'), 'score' => '0']);
-        User::create(['username' => 'u10', 'mail' => 'u10@x.x', 'pass' => bcrypt('u0'), 'score' => '0']);
-        User::create(['username' => 'u11', 'mail' => 'u11@x.x', 'pass' => bcrypt('u1'), 'score' => '0']);
-        User::create(['username' => 'u12', 'mail' => 'u12@x.x', 'pass' => bcrypt('u2'), 'score' => '0']);
-        User::create(['username' => 'u13', 'mail' => 'u13@x.x', 'pass' => bcrypt('u3'), 'score' => '0']);
-        User::create(['username' => 'u14', 'mail' => 'u14@x.x', 'pass' => bcrypt('u4'), 'score' => '0']);
-        User::create(['username' => 'u15', 'mail' => 'u15@x.x', 'pass' => bcrypt('u5'), 'score' => '0']);
-        User::create(['username' => 'u16', 'mail' => 'u16@x.x', 'pass' => bcrypt('u6'), 'score' => '0']);
-        User::create(['username' => 'u17', 'mail' => 'u17@x.x', 'pass' => bcrypt('u7'), 'score' => '0']);
-        User::create(['username' => 'u18', 'mail' => 'u18@x.x', 'pass' => bcrypt('u8'), 'score' => '0']);
-        User::create(['username' => 'u19', 'mail' => 'u19@x.x', 'pass' => bcrypt('u9'), 'score' => '0']);
-        User::create(['username' => 'u110', 'mail' => 'u110@x.x', 'pass' => bcrypt('u0'), 'score' => '0']);
-        User::create(['username' => 'u111', 'mail' => 'u111@x.x', 'pass' => bcrypt('u1'), 'score' => '0']);
-        User::create(['username' => 'u112', 'mail' => 'u112@x.x', 'pass' => bcrypt('u2'), 'score' => '0']);
-        User::create(['username' => 'u113', 'mail' => 'u113@x.x', 'pass' => bcrypt('u3'), 'score' => '0']);
-        User::create(['username' => 'u114', 'mail' => 'u114@x.x', 'pass' => bcrypt('u4'), 'score' => '0']);
-        User::create(['username' => 'u115', 'mail' => 'u115@x.x', 'pass' => bcrypt('u5'), 'score' => '0']);
-        User::create(['username' => 'u116', 'mail' => 'u116@x.x', 'pass' => bcrypt('u6'), 'score' => '0']);
-        User::create(['username' => 'u117', 'mail' => 'u117@x.x', 'pass' => bcrypt('u7'), 'score' => '0']);
-        User::create(['username' => 'u118', 'mail' => 'u118@x.x', 'pass' => bcrypt('u8'), 'score' => '0']);
-        User::create(['username' => 'u119', 'mail' => 'u119@x.x', 'pass' => bcrypt('u9'), 'score' => '0']);
+
+        User::create(['username' => 'Sarah', 'mail' => 'SarahRWills@dayrep.com ', 'pass' => bcrypt('Sarah'), 'score' => '7']);
+        User::create(['username' => 'Simone', 'mail' => 'SimoneStap@teleworm.us', 'pass' => bcrypt('Simone'), 'score' => '12']);
+        User::create(['username' => 'Roef', 'mail' => 'RoefSoetens@jourrapide.com', 'pass' => bcrypt('Roef'), 'score' => '14']);
+        User::create(['username' => 'Rahim', 'mail' => 'RahimFloor@teleworm.us', 'pass' => bcrypt('Rahim'), 'score' => '4']);
+
+
+        User::create(['username' => 'Ikra', 'mail' => 'IkraUlijn@armyspy.com', 'pass' => bcrypt('Ikra'), 'score' => '9']);
+        User::create(['username' => 'Marise', 'mail' => 'MariseVeger@armyspy.com', 'pass' => bcrypt('Marise'), 'score' => '10']);
+        User::create(['username' => 'Zilan', 'mail' => 'ZilanHebben@teleworm.us', 'pass' => bcrypt('Zilan'), 'score' => '20']);
+        User::create(['username' => 'Devin', 'mail' => 'DevinHermelink@jourrapide.com', 'pass' => bcrypt('Devin'), 'score' => '19']);
+        User::create(['username' => 'Fiep', 'mail' => 'FiepFloor@dayrep.com', 'pass' => bcrypt('Fiep'), 'score' => '15']);
+        User::create(['username' => 'Sue-Ann', 'mail' => 'Sue-AnnMoll@dayrep.com', 'pass' => bcrypt('Sue-Ann'), 'score' => '2']);
+
+
+        User::create(['username' => 'Evie', 'mail' => 'EvieBerden@jourrapide.com', 'pass' => bcrypt('Evie'), 'score' => '0']);
+        User::create(['username' => 'Carel', 'mail' => 'CarelVersteegen@jourrapide.com', 'pass' => bcrypt('Carel'), 'score' => '10']);
+        User::create(['username' => 'Anieke', 'mail' => 'AniekeKuijs@jourrapide.com', 'pass' => bcrypt('Anieke'), 'score' => '8']);
+        User::create(['username' => 'Sunaina', 'mail' => 'SunainaVerheggen@jourrapide.com', 'pass' => bcrypt('Sunaina'), 'score' => '30']);
+        User::create(['username' => 'Azzeddine', 'mail' => 'AzzeddineOosterink@teleworm.us', 'pass' => bcrypt('Azzeddine'), 'score' => '21']);
+
+        User::create(['username' => 'Fedor', 'mail' => 'FedorvanGool@armyspy.com', 'pass' => bcrypt('Fedor'), 'score' => '18']);
+        User::create(['username' => 'Alican', 'mail' => 'AlicanvanLeusen@armyspy.com', 'pass' => bcrypt('Alican'), 'score' => '20']);
+        User::create(['username' => 'Patty', 'mail' => 'PattyBrak@dayrep.com', 'pass' => bcrypt('Patty'), 'score' => '19']);
+        User::create(['username' => 'Shivanie', 'mail' => 'ShivanieSterenborg@armyspy.com', 'pass' => bcrypt('Shivanie'), 'score' => '13']);
+        User::create(['username' => 'Menno', 'mail' => 'MennoWesteneng@armyspy.com', 'pass' => bcrypt('Menno'), 'score' => '12']);
+
+        User::create(['username' => 'Shahin', 'mail' => 'ShahinvandenDungen@jourrapide.com', 'pass' => bcrypt('Shahin'), 'score' => '0']);
+        User::create(['username' => 'Jorrick', 'mail' => 'JorrickTrommelen@teleworm.us', 'pass' => bcrypt('Jorrick'), 'score' => '27']);
+        User::create(['username' => 'Iyas', 'mail' => 'IyasRafidAswad@armyspy.com', 'pass' => bcrypt('Iyas'), 'score' => '8']);
+        User::create(['username' => 'Ikraam', 'mail' => 'IkraamAsimahMaalouf@dayrep.com', 'pass' => bcrypt('Ikraam'), 'score' => '22']);
+        User::create(['username' => 'Abdel', 'mail' => 'AbdelKazimMorcos@rhyta.com', 'pass' => bcrypt('Abdel'), 'score' => '21']);
+        User::create(['username' => 'Stephan', 'mail' => 'StephanWannemaker@jourrapide.com', 'pass' => bcrypt('Stephan'), 'score' => '0']);
+
+
+        User::create(['username' => 'Karin', 'mail' => 'KarinEichelberger@rhyta.com', 'pass' => bcrypt('Karin'), 'score' => '25']);
+        User::create(['username' => 'Laurene', 'mail' => 'LaurenePatel@jourrapide.com', 'pass' => bcrypt('Laurene'), 'score' => '0']);
+        User::create(['username' => 'Soren', 'mail' => 'SorenChalifour@teleworm.us', 'pass' => bcrypt('Soren'), 'score' => '30']);
+        User::create(['username' => 'Fauna', 'mail' => 'FaunaLeroy@jourrapide.com', 'pass' => bcrypt('Fauna'), 'score' => '0']);
 
         //For each user, create a conversation with themselves
 
